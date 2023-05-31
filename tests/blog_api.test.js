0698e4151822579d0e82f1a2511f5216bab75fc0
Santeri <santeri.ora@tuni.fi>
2023-05-31 15:14:23 +0300

    part4 backend update step1-2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Moi',
+    author: 'Matti Meikäläinen',
+    url: 'http://google.com',
+    likes: 11,
+  },
+  {
+    title: 'Moi2',
+    author: 'Matti Meikäläinen2',
+    url: 'http://google.com',
+    likes: 1,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 describe('GET /api/blogs', () => {
   test('blogs are returned as json', async () => {
@@ -13,7 +37,7 @@ describe('GET /api/blogs', () => {
   })
 
   test('returns blogs with an "id" field', async () => {
-    const response = await api.get('/api/blogs')
+    const response = await api.get('/api/blogs').expect(200)
 
     response.body.forEach((blog) => {
       expect(blog.id).toBeDefined()
@@ -23,7 +47,7 @@ describe('GET /api/blogs', () => {
 
 describe('POST /api/blogs', () => {
   test('a new blog can be added', async () => {
-    const blogsBeforeAddition = await api.get('/api/blogs')
+    const blogsBeforeAddition = await api.get('/api/blogs').expect(200)
 
     const newBlog = {
       title: 'Moi',
@@ -34,7 +58,7 @@ describe('POST /api/blogs', () => {
 
     await api.post('/api/blogs').send(newBlog).expect(201)
 
-    const blogsAfterAddition = await api.get('/api/blogs')
+    const blogsAfterAddition = await api.get('/api/blogs').expect(200)
 
     expect(blogsAfterAddition.body).toHaveLength(blogsBeforeAddition.body.length + 1)
   })
@@ -65,6 +89,44 @@ describe('POST /api/blogs', () => {
 
     await api.post('/api/blogs').send(blogWithoutTitle).expect(400)
     await api.post('/api/blogs').send(blogWithoutUrl).expect(400)
+  })
+})
+
+describe('DELETE /api/blogs/:id', () => {
+  test('blog can be deleted', async () => {
+    const response = await api.get('/api/blogs').expect(200)
+    const blogsBeforeDeletion = response.body
+
+    const blogToDelete = blogsBeforeDeletion[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAfterDeletion = await api.get('/api/blogs').expect(200)
+
+    expect(blogsAfterDeletion).not.toContain(blogToDelete.content)
+  })
+})
+
+describe('PUT /api/blogs/:id', () => {
+  test('likes of blog can be updated', async () => {
+    const response = await api.get('/api/blogs').expect(200)
+    const blogsBeforeUpdate = response.body
+
+    const blogToUpdate = blogsBeforeUpdate[0]
+
+    const updatedLikes = blogToUpdate.likes + 10
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({ ...blogToUpdate, likes: updatedLikes })
+      .expect(200)
+
+    const updatedBlogsResponse = await api.get('/api/blogs').expect(200)
+    const updatedBlogs = updatedBlogsResponse.body
+
+    const updatedBlog = updatedBlogs.find(blog => blog.id === blogToUpdate.id)
+
+    expect(updatedBlog.likes).toEqual(updatedLikes)
   })
 })
 
